8f0e8d079f11f42390a201dab4d9623ffc51d6b0
Meiryanne Martins <meirympereira@gmail.com>
2026-04-23 16:58:32 -0300

    feat: find total on orders by customer id

--- src/main/java/tech/buildrun/btgpactual/orderms/controller/OrderController.java
+++ src/main/java/tech/buildrun/btgpactual/orderms/controller/OrderController.java
@@ -2,6 +2,7 @@ package tech.buildrun.btgpactual.orderms.controller;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import tech.buildrun.btgpactual.orderms.controller.dto.ApiResponse;
 import tech.buildrun.btgpactual.orderms.controller.dto.OrderResponse;
 import tech.buildrun.btgpactual.orderms.controller.dto.PaginationResponse;
 import tech.buildrun.btgpactual.orderms.service.OrderService;
+
+import java.util.Map;
 
 @RestController
 public class OrderController {
@@ -18,12 +21,15 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @GetMapping("/customers/{customerId}/orders")
     public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@PathVariable Long customerId,
                                                                  @RequestParam(name = "page", defaultValue = "0") Integer page,
 
                                                                  @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
         var pageResponse = orderService.findAllByCustomerId(customerId, PageRequest.of(page, pageSize));
+        var totalOnOrders = orderService.findTotalOnOrdersByCustomerId(customerId);
         return ResponseEntity.ok(new ApiResponse<>(
+                Map.of("totalOnOrders", totalOnOrders),
                 pageResponse.getContent(),
                 PaginationResponse.fromPage(pageResponse)
 
--- src/main/java/tech/buildrun/btgpactual/orderms/service/OrderService.java
+++ src/main/java/tech/buildrun/btgpactual/orderms/service/OrderService.java
@@ -1,7 +1,10 @@
 package tech.buildrun.btgpactual.orderms.service;
 
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 import tech.buildrun.btgpactual.orderms.controller.dto.OrderResponse;
 import tech.buildrun.btgpactual.orderms.entity.OrderEntity;
@@ -11,13 +14,19 @@ import tech.buildrun.btgpactual.orderms.repository.OrderRepository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository,
+                        MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event){
@@ -48,5 +57,16 @@ public class OrderService {
         var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
 
         return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId) {
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
     }
 }
